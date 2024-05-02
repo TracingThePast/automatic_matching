d9045ea003e4e9736fe33278a70479445a63cb16
Jan Bernrader <jbernrader@web.de>
2024-05-02 14:53:25 +0200

    Created a installable package for pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='automatic_matching',
+    version='0.1.0',    
+    description='An automatic matching algorithm that allows to link references to the same person in multiple independent databases, based on a list of search criteria.',
+    url='https://github.com/TracingThePast/automatic_matching',
+    author='Jan Bernrader',
+    author_email='j.bernrader@tracingthepast.org',
+    license='MIT',
+    packages=['automatic_matching'],
+    install_requires=['levenshtein~=0.25',
+                      'numpy~=1.26',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT',  
+        'Operating System :: POSIX :: Linux',   
+        'Programming Language :: Python :: 3.11',
+    ],
+)
